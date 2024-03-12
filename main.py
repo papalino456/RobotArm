c41e648729383f7cc23ae1d02cc15e4fd8578a4e
Sebastian Barrio <sebastian.barrio.b@gmail.com>
2024-03-12 11:17:02 -0600

    add newly calculated theta to main and correctly clamp cos value, add cube rotation and open value parameters to calculate joint function

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def sendAngles(angles):
     print(angles)
     print('Received', repr(data.decode('utf-8')))
 
-def calculateJoints(x,y,z):
+
+def calculateJoints(x,y,z,cubeRot=0,bOpen=False):
     A1=55.0
     A2=30.0
     A3=200.0
@@ -21,14 +22,18 @@ def calculateJoints(x,y,z):
     actual_distance = math.sqrt(x**2 + y**2 + z**2)
     # Clamp the x, y, z values if they are outside the semi-sphere domain
     if actual_distance > max_radius:
-        scaling_factor = max_radius / actual_distance
-        x *= scaling_factor
-        y *= scaling_factor
-        z *= scaling_factor
+        print("Point outside range, clamping x, y, z values")
+
+    cosTheta3 = max(min(((x**2+y**2+z**2-A3**2-A4**2)/(2*A3*A4)), 1), -1)
+    senTheta3 = -math.sqrt(1-cosTheta3**2)
     theta1 = math.degrees(math.atan2(y,x))
-    theta2 = math.degrees(math.atan2(z,x) + math.acos((A3**2+(x**2+z**2)-(A4+A5)**2)/(2*A3*(math.sqrt(x**2+z**2)))))
-    theta3 = -(math.degrees(math.acos((A3**2+(A4+A5)**2-(math.sqrt(x**2+z**2))**2)/(2*A3*(A4+A5))) - math.pi))
-    joints = [theta1, theta2, theta3, 0, 0]
+    #theta2 = math.degrees(math.atan2(z,x) + math.acos((A3**2+(x**2+z**2)-(A4+A5)**2)/(2*A3*(math.sqrt(x**2+z**2))))) #OLD theta 
+    theta2 = math.degrees(math.atan2(z,math.sqrt(x**2+y**2))-math.atan2(A4*senTheta3,A3+A4*cosTheta3))
+    #theta3 = -(math.degrees(math.acos((A3**2+(A4+A5)**2-(math.sqrt(x**2+z**2))**2)/(2*A3*(A4+A5))) - math.pi)) #OLD theta
+    theta3 = -(math.degrees((math.atan2(senTheta3,cosTheta3))))
+    #print(theta1, theta2, theta3)
+    openVal = 90 if bOpen else 0
+    joints = [theta1, theta2, theta3, cubeRot, openVal]
     return joints
 
 def processImage(frame):
@@ -71,14 +76,18 @@ def processImage(frame):
     return coordList
 
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect(('192.168.43.181', 12345))
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if ret:
-        processImage(frame)
+        coordList = processImage(frame)
+        try:
+            calculateJoints(coordList[0][0], coordList[0][1], 0)
+        except IndexError:
+            print("No object found")
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 cap.release()
